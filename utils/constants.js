e36e47913813821e46112ae6cb5feba577f2ef4d
Tran Que An <96300746+tquean15012003@users.noreply.github.com>
2023-03-24 12:21:06 +0800

    update api + localize database

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
-const url = "https://quean001.iex.cloud/v1/data/core/quote";
+const url = "https://api.iex.cloud/v1/data/CORE/QUOTE";
 const imageUrl = "https://storage.googleapis.com/iex/api/logos"
 
-const token = "pk_88d98b3cff4b4b8cb421d81560ff1b8f";
+const token = "sk_8b97d632bdf047f7ac070840001c5173";
 
 module.exports = {
     url,
